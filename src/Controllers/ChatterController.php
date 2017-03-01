79c8e271de233bcfdcb1d403e97917868c17b2b7
stefandb <stefan@esser-emmerik.nl>
2017-03-01 14:33:19 +0100

    PROC-1140 remove pagination
    this breaks the group filter query

--- src/Controllers/ChatterController.php
+++ src/Controllers/ChatterController.php
@@ -14,11 +14,11 @@ class ChatterController extends Controller
         $pagination_results = config('chatter.paginate.num_of_results');
 
 
-    	$discussions = Models::discussion()->with('user')->with('post')->with('postsCount')->with('category')->orderBy('created_at', 'DESC')->paginate($pagination_results);
+    	$discussions = Models::discussion()->with('user')->with('post')->with('postsCount')->with('category')->orderBy('created_at', 'DESC')->get();
     	if(isset($slug)){
     		$category = Models::category()->where('slug', '=', $slug)->first();
     		if(isset($category->id)){
-    			$discussions = Models::discussion()->with('user')->with('post')->with('postsCount')->with('category')->where('chatter_category_id', '=', $category->id)->orderBy('created_at', 'DESC')->paginate($pagination_results);
+    			$discussions = Models::discussion()->with('user')->with('post')->with('postsCount')->with('category')->where('chatter_category_id', '=', $category->id)->orderBy('created_at', 'DESC')->get();
     		}
     	}
 
