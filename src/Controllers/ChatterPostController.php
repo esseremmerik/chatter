0c55a1d377dfd97b72241e090b38d72f67e600c6
stefandb <stefan@esser-emmerik.nl>
2017-08-09 13:47:27 +0200

    update gate check

--- src/Controllers/ChatterPostController.php
+++ src/Controllers/ChatterPostController.php
@@ -210,10 +210,10 @@ class ChatterPostController extends Controller
     {
         $post = Models::post()->with('discussion')->findOrFail($id);
 
-        if ($request->user()->id !== (int) $post->user_id || !Gate::allows('remove-others-forum-comments')) {
+        if ($request->user()->id !== (int) $post->user_id || !\Illuminate\Support\Facades\Gate::allows('remove-others-forum-comments')) {
 
             $defaultUrl = '/'.config('chatter.routes.home');
-            $eventClass = app()->make('DevDojo\Chatter\Events\RedirectUrl', [$request, null, $defaultUrl, 'home']);
+            $eventClass = app()->make('DevDojo\Chatter\Events\RedirectUrl', [$request, $post->discussion, $defaultUrl, 'home']);
             Event::fire($eventClass);
 
             return redirect($eventClass->redirectUrl)->with([
@@ -227,7 +227,7 @@ class ChatterPostController extends Controller
             $post->discussion()->delete();
 
             $defaultUrl = '/'.config('chatter.routes.home');
-            $eventClass = app()->make('DevDojo\Chatter\Events\RedirectUrl', [$request, null, $defaultUrl, 'home']);
+            $eventClass = app()->make('DevDojo\Chatter\Events\RedirectUrl', [$request, $post->discussion, $defaultUrl, 'home']);
             Event::fire($eventClass);
 
             return redirect($eventClass->redirectUrl)->with([
